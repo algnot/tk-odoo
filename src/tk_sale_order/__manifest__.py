b33cc903aab8fa224e88b792ebab633bf26cfb1b
algnot <thanawat.tongla@gmail.com>
2026-03-24 20:36:08 +0700

    fix up

--- src/tk_sale_order/__manifest__.py
+++ src/tk_sale_order/__manifest__.py
@@ -1,9 +1,10 @@
 {
     "name": "Tonkla Sale Report",
     "version": "1.0",
-    "depends": ["sale", "web"],
+    "depends": ["sale", "account", "web"],
     "data": [
         "views/sale_report.xml",
+        "views/invoice_report.xml",
     ],
     "installable": True,
     "auto_install": False,
